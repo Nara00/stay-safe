c4c2590044562f7a47efda97b67e2640e021491e
ticicobresiserr <tici.cobresi21@gmail.com>
2022-06-09 13:03:18 -0300

    listo

--- frontend/src/componentes/dashboard.js
+++ frontend/src/componentes/dashboard.js
@@ -7,37 +7,39 @@ import "./dashboard.css"
 import PieChart from "./Charts/PieChart";
 
 function Dashboard() {
-    const [idDias, setIdDias] = useState(0)
+    const [idDias, setIdDias] = useState(-1)
     const [crimesData, setCrimeData] = hookDashbard(idDias)
     // console.log('mio', crimesData);
 
     return (
-        <div class="page contact-us-page">
-            <section class="clean-block clean-form dark">
-                <div class="chartCard">
-                    <select class="form-select" id="name"
-                        name="type">
-                        <option hidden disabled selected value >Filtrar por fechas...</option>
-                        <option defaultChecked value={"Todos"} onClick={() => { setIdDias(0) }}>Todos</option>
-                        <option value={"Dia"} onClick={() => { setIdDias(1) }} >Hoy</option>
-                        <option value={"Semana"} onClick={() => { setIdDias(8) }}>Semana</option>
-                        <option value={"Mes"} onClick={() => { setIdDias(30) }}>Mes</option>
-                        <option value={"Anio"} onClick={() => { setIdDias(365) }}>Anio</option>
-                    </select>
-                    <div class='chartmenu p'>
-                        <div class="chartCard">
-                            <PieChart chartData={crimesData} />
-                        </div>
-                        <div class="chartCard">
-                        </div>
-                        <div class="chartCard">
-                            <LineChart chartData={crimesData} />
+        <>
+            <div class="page contact-us-page">
+                <section class="clean-block clean-form dark">
+                    <div class="chartCard">
+                        <div className="dash">DASHBOARD DE CRIMENES</div>
+                        <select class="form-select" id="name"
+                            name="type">
+                            <option hidden disabled selected value >Filtrar por fechas...</option>
+                            <option defaultChecked value={"Todos"} onClick={() => { setIdDias(-1) }}>Todos</option>
+                            <option value={"Dia"} onClick={() => { setIdDias(0) }} >Hoy</option>
+                            <option value={"Semana"} onClick={() => { setIdDias(6) }}>Semana</option>
+                            <option value={"Mes"} onClick={() => { setIdDias(30) }}>Mes</option>
+                            <option value={"Anio"} onClick={() => { setIdDias(365) }}>Anio</option>
+                        </select>
+                        <div class='chartmenu p'>
+                            <div class="chartCard">
+                                <PieChart chartData={crimesData} />
+                            </div>
+                            <div class="chartCard">
+                            </div>
+                            <div class="chartCard">
+                                <LineChart chartData={crimesData} />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section >
-        </div >
-
+                </section >
+            </div >
+        </>
 
     );
 }
